b56210f3c05e7fe32061fdc994b4727d6e489de8
Paulo Henrique <gladgamer01@gmail.com>
2022-03-12 14:44:52 -0300

    test: ensure signUp throws 400 if no cellphone is provided

--- src/domain/repositories/user/functions/user/signUp/signUp.spec.ts
+++ src/domain/repositories/user/functions/user/signUp/signUp.spec.ts
@@ -3,7 +3,7 @@ import { httpRequest } from "@interfaces/http";
 import { signUp } from ".";
 
 describe('signUp', () => {
-  it('should throw an error if no email is passed',async () => {
+  it('should throw an error if no email is provided',async () => {
     const response = await signUp(<httpRequest> {
       body: {
         name: 'Paulo Henrique'
@@ -13,7 +13,7 @@ describe('signUp', () => {
     expect(response.body).toEqual(new MissingParamError('email'))
   }) 
 
-  it('should throw an error if no name is passed',async () => {
+  it('should throw an error if no name is provided',async () => {
     const response = await signUp(<httpRequest> {
       body: {
         email: 'paulo@blocksrvt.com'
@@ -23,7 +23,7 @@ describe('signUp', () => {
     expect(response.body).toEqual(new MissingParamError('name'))
   }) 
 
-  it('should throw an error if no cpf is passed',async () => {
+  it('should throw an error if no cpf is provided',async () => {
     const response = await signUp(<httpRequest> {
       body: {
         email: 'paulo@blocksrvt.com',
@@ -34,7 +34,7 @@ describe('signUp', () => {
     expect(response.body).toEqual(new MissingParamError('cpf'))
   }) 
 
-  it('should throw an error if no address is passed',async () => {
+  it('should throw an error if no address is provided',async () => {
     const response = await signUp(<httpRequest> {
       body: {
         email: 'paulo@blocksrvt.com',
@@ -44,6 +44,19 @@ describe('signUp', () => {
     });
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('address'))
+  }) 
+
+  it('should throw an error if no cellphone is provided',async () => {
+    const response = await signUp(<httpRequest> {
+      body: {
+        email: 'paulo@blocksrvt.com',
+        name: 'Paulo Henrique',
+        cpf: '321.232.356-99',
+        address: 'R do Joao N358'
+      }
+    });
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('cellphone'))
   }) 
 
 })
