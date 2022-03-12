bfdf71353e625812808238bf6be3746edd1e3667
Paulo Henrique <gladgamer01@gmail.com>
2022-03-12 16:55:04 -0300

    feat: add logs on badRequests

--- src/domain/helpers/http-helper.ts
+++ src/domain/helpers/http-helper.ts
@@ -1,6 +1,10 @@
-import { httpResponse } from "@interfaces/http"
+import logger from '@domain/log/logger';
+import { httpResponse } from '@interfaces/http';
 
-export const badRequest = (error: Error) : httpResponse => ({
+export const badRequest = (error: Error): httpResponse => {
+  logger.error(error.message);
+  return {
     statusCode: 400,
-    body: error
-})
+    body: error,
+  };
+};
