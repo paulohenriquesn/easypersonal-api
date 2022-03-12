eb0275b3e0ba5b0497eabff072e7f254639cbc0f
Paulo Henrique <gladgamer01@gmail.com>
2022-03-12 18:31:55 -0300

    test: ensure google will validate it tokens

--- src/domain/repositories/user/functions/user/signUp/signUp.spec.ts
+++ src/domain/repositories/user/functions/user/signUp/signUp.spec.ts
@@ -1,12 +1,17 @@
 import { MissingParamError, ParamInvalid, SchemaInvalid } from "@errors/index";
 import { httpRequest } from "@interfaces/http";
+import { validateToken } from "@repositories/user/providers/google/verifyToken";
 import { signUp } from ".";
 
 describe('signUp', () => {
   it('throw an error if no email is provided',async () => {
     const response = await signUp(<httpRequest> {
       body: {
-        name: 'Paulo Henrique'
+        name: 'Paulo Henrique',
+        cpf: '869.382.480-52',
+        address: 'Rua do Joao N35',
+        cellphone: '11999226498',
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
@@ -16,7 +21,11 @@ describe('signUp', () => {
   it('throw an error if no name is provided',async () => {
     const response = await signUp(<httpRequest> {
       body: {
-        email: 'paulo@blocksrvt.com'
+        email: 'paulo@blocksrvt.com',
+        cpf: '869.382.480-52',
+        address: 'Rua do Joao N35',
+        cellphone: '11999226498',
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
@@ -28,6 +37,9 @@ describe('signUp', () => {
       body: {
         email: 'paulo@blocksrvt.com',
         name: 'Paulo Henrique',
+        address: 'Rua do Joao N35',
+        cellphone: '11999226498',
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
@@ -39,7 +51,9 @@ describe('signUp', () => {
       body: {
         email: 'paulo@blocksrvt.com',
         name: 'Paulo Henrique',
-        cpf: '321.232.356-99'
+        cpf: '869.382.480-52',
+        cellphone: '11999226498',
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
@@ -51,8 +65,9 @@ describe('signUp', () => {
       body: {
         email: 'paulo@blocksrvt.com',
         name: 'Paulo Henrique',
-        cpf: '321.232.356-99',
-        address: 'R do Joao N358'
+        cpf: '869.382.480-52',
+        address: 'Rua do Joao N35',
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
@@ -66,7 +81,8 @@ describe('signUp', () => {
         name: 'Paulo Henrique',
         cpf: '321.232.356-99',
         address: 'R do Joao N358',
-        cellphone: 123
+        cellphone: 123,
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
@@ -80,11 +96,29 @@ describe('signUp', () => {
         name: 'Paulo Henrique',
         cpf: '321.232.356-99',
         address: 'R do Joao N358',
-        cellphone: "99999999"
+        cellphone: "99999999",
+        google_token: 'token'
       }
     });
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new ParamInvalid('cpf'))
+  }) 
+
+  it('throw an error if no google_token is provided',async () => {
+    const response = await signUp(<httpRequest> {
+      body: {
+        email: 'paulo@blocksrvt.com',
+        name: 'Paulo Henrique',
+        cpf: '869.382.480-52',
+        address: 'R do Joao N358'
+      }
+    });
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('cellphone'))
+  }) 
+
+  it('throw an error if no google_token is invalid',async () => {
+      expect(await validateToken('teste')).toBe(false)
   }) 
 
 })
