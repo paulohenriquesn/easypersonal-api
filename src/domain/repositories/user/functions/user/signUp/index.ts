5460530e1c6f8839180f63f008d32bae85238b54
Paulo Henrique <gladgamer01@gmail.com>
2022-03-12 14:32:11 -0300

    feat: ensure signUp throws 400 if no email is provided

---
+++ src/domain/repositories/user/functions/user/signUp/index.ts
@@ -0,0 +1,21 @@
+import { MissingParamError } from "@errors/MissingParamError";
+import { badRequest } from "@helpers/http-helper";
+import { httpResponse } from "@interfaces/http";
+
+export async function signUp(request) : Promise<httpResponse> {
+  const requiredFields = [
+    "email",
+  ];
+
+  for(const field of requiredFields) {
+    if(request.body[field] === undefined) {
+      return badRequest(new MissingParamError(field))
+    }
+  }
+
+  return <httpResponse>{
+    statusCode: 200,
+    message: 'ok',
+    body: {}
+  }
+} 
