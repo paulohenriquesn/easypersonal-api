f91825dd88401281e2fa0ca8cff2df98c3c834e9
Paulo Henrique <gladgamer01@gmail.com>
2022-03-12 18:05:54 -0300

    feat: sure google will verify its token to auth

---
+++ src/domain/repositories/user/providers/google/verifyToken.ts
@@ -0,0 +1,21 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
+const client = new OAuth2Client(CLIENT_ID);
+
+export async function validateToken(token: string): Promise<boolean> {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID,
+  });
+
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+
+  if (userid) {
+    return true;
+  }
+
+  return false;
+}
