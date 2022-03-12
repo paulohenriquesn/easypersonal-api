740f6d22e2bdfaef88e225db0714aa5c8b4ccf17
Paulo Henrique <gladgamer01@gmail.com>
2022-03-12 14:29:47 -0300

    chore: config paths map

--- jest.config.ts
+++ jest.config.ts
@@ -7,4 +7,13 @@ export default {
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '^@domain/(.*)$': '<rootDir>/src/$1',
+    '^@repositories/(.*)$': '<rootDir>/src/domain/repositories/$1',
+    '^@interfaces/(.*)$': '<rootDir>/src/domain/interfaces/$1',
+    '^@helpers/(.*)$': '<rootDir>/src/domain/helpers/$1', 
+    '^@controllers/(.*)$': '<rootDir>/src/domain/controllers/$1',   
+    '^@errors/(.*)$': '<rootDir>/src/domain/errors/$1', 
+  }
+
 };
