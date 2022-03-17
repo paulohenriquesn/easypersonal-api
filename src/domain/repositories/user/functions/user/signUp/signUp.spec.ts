59ff4f6c9bc6f22915d90293c1956c9c891f59c1
Paulo Henrique <gladgamer01@gmail.com>
2022-03-17 17:48:34 -0300

    chore: remove birthday as required param

--- src/domain/repositories/user/functions/user/signUp/signUp.spec.ts
+++ src/domain/repositories/user/functions/user/signUp/signUp.spec.ts
@@ -6,7 +6,6 @@ describe('signUp', () => {
   it('throw an error if no email is provided', async () => {
     const response = await signUp('trainer', {
       full_name: 'paulo',
-      birthday: '02-07-2003',
     });
     expect(response).toEqual(badRequest(new MissingParamError('email')));
   });
@@ -14,16 +13,7 @@ describe('signUp', () => {
   it('throw an error if no full_name is provided', async () => {
     const response = await signUp('trainer', {
       email: 'paulo@blocksrvt.com',
-      birthday: '02-07-2003',
     });
     expect(response).toEqual(badRequest(new MissingParamError('full_name')));
-  });
-
-  it('throw an error if no birthday is provided', async () => {
-    const response = await signUp('trainer', {
-      email: 'paulo@blocksrvt.com',
-      full_name: 'Paulo Henrique',
-    });
-    expect(response).toEqual(badRequest(new MissingParamError('birthday')));
   });
 });
