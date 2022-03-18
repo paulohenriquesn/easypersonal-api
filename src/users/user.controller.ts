dc4750141bf09332c5691c16163414b4746fcc00
Paulo Henrique <gladgamer01@gmail.com>
2022-03-17 21:40:14 -0300

    feat: ensure user can fetch its information using get route

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Post,
+  Res,
+  UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ApiBody } from '@nestjs/swagger';
 import { authUserDto } from './swagger/authUser';
 import { UserService } from './user.service';
@@ -13,5 +22,12 @@ export class UserController {
   })
   async auth(@Body() body, @Res() res) {
     return this.userService.authUser(body, res);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/')
+  async fetch(@Headers() headers, @Res() res) {
+    const token = headers.authorization;
+    return this.userService.fetchUser(token, res);
   }
 }
